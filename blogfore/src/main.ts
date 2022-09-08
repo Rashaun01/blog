d046e64d8b022f2999a07d1408e8e6835ea63081
rashaun <1780953733@qq.com>
2022-09-08 18:17:34 +0800

    feat:[master] 项目前端配置补充

--- blogfore/src/main.ts
+++ blogfore/src/main.ts
@@ -1,6 +1,8 @@
 import { createApp } from 'vue'
+import { useI18n } from 'vue-i18n'
 import App from './App.vue'
 import router from './router'
 import store from './store'
 
-createApp(App as never).use(store).use(router).mount('#app')
+createApp(App as never).use(store).use(router).use(useI18n)
+  .mount('#app')
