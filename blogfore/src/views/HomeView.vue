b3abc7fe08ff2f54148cc2e05349f951c91e14b1
rashaun <1780953733@qq.com>
2022-09-09 03:03:05 +0800

    feat:[master] 解决element+无法导入问题，并加入了按需自动导入element+组件

--- blogfore/src/views/HomeView.vue
+++ blogfore/src/views/HomeView.vue
@@ -1,5 +1,13 @@
 <template>
   <div class="home">
+    <el-row>
+      <el-col :span="24">
+        <el-avatar :size="50" :src="'https://cube.elemecdn.com/0/88/03b0d39583f48206768a7534e55bcpng.png'" />
+      </el-col>
+      <el-col :span="24">
+        <el-rate v-model="value" size="large" />
+      </el-col>
+    </el-row>
     <img alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
   </div>
@@ -12,6 +20,11 @@ import HelloWorld from '@/components/HelloWorld.vue' // @ is an alias to /src
 @Options({
   components: {
     HelloWorld
+  },
+  data() {
+    return {
+      value: 0
+    }
   }
 })
 export default class HomeView extends Vue {}
